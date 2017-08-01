a1344f6178721b667d43d53b4f57eff1037bb1af
Norman Zhi <normanzyb@gmail.com>
2017-08-01 10:34:38 +0100

    Fix for wchar_t support

--- include/spdlog/details/logger_impl.h
+++ include/spdlog/details/logger_impl.h
@@ -10,7 +10,7 @@
 
 #include <memory>
 #include <string>
-
+#include <locale>
 
 // create logger with given name, sinks and the default pattern formatter
 // all other ctors will call this one
@@ -399,7 +399,7 @@ inline void spdlog::logger::log_if(const bool flag, level::level_enum lvl, const
 {
     if (flag)
     {
-        log(lvl, fmt, args);
+	log(lvl, fmt, args...);
     }
 }
 
